e2cdc1bc45f3e85faa164e7be6d37fbbf9e5a65f
dp160610 <46530061+dp160610@users.noreply.github.com>
2020-11-15 08:13:32 +0530

    Updates

---
+++ build/precache-manifest.ae4c595fb2aa232c000bec135f3a598e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e3186139d060b3c6e5b849a5a6d9c6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59d1eb2bbad872b22334",
+    "url": "/static/css/main.8c3f8730.chunk.css"
+  },
+  {
+    "revision": "91de040d1adf9625559e",
+    "url": "/static/js/2.5c1e1951.chunk.js"
+  },
+  {
+    "revision": "4472b9b1c403804b7335229d4dbf4deb",
+    "url": "/static/js/2.5c1e1951.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59d1eb2bbad872b22334",
+    "url": "/static/js/main.01eac345.chunk.js"
+  },
+  {
+    "revision": "0f6e3c7566b6812736c8",
+    "url": "/static/js/runtime-main.00d46714.js"
+  }
+]);
